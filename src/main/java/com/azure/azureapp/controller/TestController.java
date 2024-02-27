a3d8809b7025d960a4e9efcee640dbf8657c0896
Lovedeep <lovedeep@happiestminds.com>
2024-02-27 11:56:11 +0530

    added app code

---
+++ src/main/java/com/azure/azureapp/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.azure.azureapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @GetMapping
+    public String getApi(){
+        return "Hello World";
+    }
+
+}
